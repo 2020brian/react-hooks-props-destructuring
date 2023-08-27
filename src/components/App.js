466527650620889039a6c9edf94e5efcbdee4cdb
onjorobrian <onjorobrian1@gmail.com>
2023-08-28 02:15:08 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,18 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
     </div>
   );
 }
-
+function MovieCard(props) {
+  return (
+    <div className="movie-card">
+      <img src={props.posterSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <small>{props.genres.join(", ")}</small>
+    </div>
+  );
+}
 export default App;
+
